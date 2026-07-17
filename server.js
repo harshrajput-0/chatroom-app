dc0506b45d3e013ba210d6bb21440a1f80f6da6b
Harsh Rajput <harshrajput0426@gmail.com>
2026-07-17 14:10:34 +0530

    fix: server port fixed

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ io.on('connection', socket => {
         });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
